0b93e4b7c3a49f75a0485b9d21cd262c8b44e241
Aman Patel <patel.26@iitj.ac.in>
2022-11-02 22:24:05 +0530

    deployed contract

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -1,34 +1,23 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-contract Lock {
-    uint public unlockTime;
-    address payable public owner;
+contract HelloWorld {
+    event update(string oldStr, string newStr);
 
-    event Withdrawal(uint amount, uint when);
+    string public message;
 
-    constructor(uint _unlockTime) payable {
-        require(
-            block.timestamp < _unlockTime,
-            "Unlock time should be in the future"
-        );
-
-        unlockTime = _unlockTime;
-        owner = payable(msg.sender);
+    constructor(string memory _initMsg) {
+        message = _initMsg;
     }
 
-    function withdraw() public {
-        // Uncomment this line, and the import of "hardhat/console.sol", to print a log in your terminal
-        // console.log("Unlock time is %o and block timestamp is %o", unlockTime, block.timestamp);
+    function updateMsg(string memory _newMsg) public {
+        string memory _oldMsg = message;
 
-        require(block.timestamp >= unlockTime, "You can't withdraw yet");
-        require(msg.sender == owner, "You aren't the owner");
+        message = _newMsg;
 
-        emit Withdrawal(address(this).balance, block.timestamp);
-
-        owner.transfer(address(this).balance);
+        emit update(_oldMsg, _newMsg);
     }
 }
