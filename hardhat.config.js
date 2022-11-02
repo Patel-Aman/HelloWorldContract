b5f8012dc9f4046e5d3cf4ba56ab80d67c2a48a3
Aman Patel <patel.26@iitj.ac.in>
2022-11-02 22:44:11 +0530

    Smart contract part finished

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,8 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('dotenv').config();
+require('@nomiclabs/hardhat-etherscan');
 
-const { API_URL, PRIVATE_KEY } = process.env;
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -13,5 +14,8 @@ module.exports = {
       url: API_URL,
       accounts: [`0x${PRIVATE_KEY}`],
     }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
   }
 };
---
+++ scripts/interact.js
@@ -0,0 +1,36 @@
+
+require('dotenv').config();
+const { ethers } = require('hardhat');
+const contract = require('../artifacts/contracts/HelloWorld.sol/HelloWorld.json');
+
+const { API_KEY, PRIVATE_KEY, CONTRACT_ADDRESS } = process.env;
+
+// provider
+const provider = new ethers.providers.AlchemyProvider(network="goerli", API_KEY);
+
+// signer
+const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+
+// contract
+const HelloWorldContract = new ethers.Contract(CONTRACT_ADDRESS, contract.abi, signer);
+
+async function main() {
+    let message = await HelloWorldContract.message();
+
+    console.log("Current message is:", message);
+
+    console.log("updating message...");
+
+    const tx = await HelloWorldContract.updateMsg("Next steps");
+    await tx.wait();
+    message = await HelloWorldContract.message();
+
+    console.log("New message is:", message);
+};
+
+main()
+.then( () => process.exit(0))
+.catch( error => {
+  console.error(error);
+  process.exit(1);
+});
